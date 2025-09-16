5b7404deafa2679daec37a52905886389fe7ccfe
Swarnavo2003 <swarnavo2020@gmail.com>
2025-09-16 21:46:27 +0530

    new ip added 2

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://13.235.33.176:5173'],
+    origin: ['http://localhost:5173', 'http://13.126.108.68:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
